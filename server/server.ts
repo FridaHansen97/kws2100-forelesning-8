d14b4e4906e52f20b52892a714e076a5ac22cc77
frida <frida1997@live.no>
2026-03-14 17:41:18 +0100

    show overlay with andress information when user clickes on map

--- server/server.ts
+++ server/server.ts
@@ -36,27 +36,26 @@ app.get("/api/kommuner/:z/:x/:y", async (c) => {
   const { x, y, z } = c.req.param();
   const result = await postgres.query(
     `
-    with mvt as (select kommunenavn,
-                        kommunenummer,
-                        st_asmvtgeom(omrade_3857, st_tileenvelope($1, $2, $3))
-                 from kommune
-                 where omrade_3857 && st_tileenvelope($1, $2, $3))
-    select st_asmvt(mvt.*) from mvt
-  `,
+      with mvt as (select kommunenavn,
+                          kommunenummer,
+                          st_asmvtgeom(omrade_3857, st_tileenvelope($1, $2, $3))
+                   from kommune
+                   where omrade_3857 && st_tileenvelope($1, $2, $3))
+      select st_asmvt(mvt.*) from mvt
+    `,
     [z, x, y],
   );
   return c.body(result.rows[0].st_asmvt, 200, {
     "Content-Type": "application/vnd.mapbox-vector-tile",
   });
 });
-
 app.get("/api/vegadresse/:z/:x/:y", async (c) => {
   const { x, y, z } = c.req.param();
 
   if (parseInt(z) < 15) {
     // return an empty mapbox vector tile response
     const result = await postgres.query(
-      `SELECT ST_AsMVT(tile,'layer_name', 4096, 'geom') FROM (SELECT NULL::geometry AS geom WHERE FALSE) AS tile`,
+      `SELECT ST_AsMVT(tile, 'layer_name', 4096, 'geom') FROM (SELECT NULL::geometry AS geom WHERE FALSE) AS tile`,
     );
     return c.body(result.rows[0].st_asmvt, 200, {
       "Content-Type": "application/vnd.mapbox-vector-tile",
@@ -65,13 +64,13 @@ app.get("/api/vegadresse/:z/:x/:y", async (c) => {
 
   const result = await postgres.query(
     `
-    with mvt as (select adresseid,
-                        adressetekst,
-                        st_asmvtgeom(representasjonspunkt_3857, st_tileenvelope($1, $2, $3))
-                 from vegadresse
-                 where representasjonspunkt_3857 && st_tileenvelope($1, $2, $3))
-    select st_asmvt(mvt.*) from mvt
-  `,
+      with mvt as (select adresseid,
+                          adressetekst,
+                          st_asmvtgeom(representasjonspunkt_3857, st_tileenvelope($1, $2, $3))
+                   from vegadresse
+                   where representasjonspunkt_3857 && st_tileenvelope($1, $2, $3))
+      select st_asmvt(mvt.*) from mvt
+    `,
     [z, x, y],
   );
   return c.body(result.rows[0].st_asmvt, 200, {
